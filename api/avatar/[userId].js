986a5a32b4e3983d5687f106eaa351a401e11b2b
Akash R <imakash566@gmail.com>
2026-06-30 10:01:54 +0530

    Update [userId].js

--- api/avatar/[userId].js
+++ api/avatar/[userId].js
@@ -3,15 +3,28 @@ import { supabase } from "../../server/lib/supabase.js";
 export default async function handler(req, res) {
   const { userId } = req.query;
 
-  const { data: user } = await supabase
+  const { data } = await supabase
     .from("tg_users")
     .select("avatar_path")
     .eq("id", String(userId))
     .single();
 
-  if (!user?.avatar_path) {
-    return res.status(404).send("No avatar");
+  if (!data?.avatar_path) {
+    return res.status(404).end();
   }
 
-  res.redirect(`https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${user.avatar_path}`);
+  const tgUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${data.avatar_path}`;
+  const img = await fetch(tgUrl);
+
+  if (!img.ok) {
+    return res.status(404).end();
+  }
+
+  const buffer = Buffer.from(await img.arrayBuffer());
+
+  res.setHeader("Content-Type", img.headers.get("content-type") || "image/jpeg");
+  res.setHeader("Cache-Control", "public, max-age=86400");
+  res.setHeader("Content-Disposition", "inline");
+
+  return res.status(200).send(buffer);
 }
